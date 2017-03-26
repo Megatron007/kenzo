f675ec3e74a9f17ea3def24b24c4b81989635b55
Omkar <djomi400@gmail.com>
2017-03-26 03:03:33 -0400

    kenzo: aosp

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -113,14 +113,14 @@ BOARD_HARDWARE_CLASS += \
 BOARD_USES_QCNE := true
 
 # Dex
-ifeq ($(HOST_OS),linux)
-  ifneq ($(TARGET_BUILD_VARIANT),eng)
-    ifeq ($(WITH_DEXPREOPT),)
-      WITH_DEXPREOPT := true
-    endif
-  endif
-endif
-WITH_DEXPREOPT_BOOT_IMG_ONLY ?= true
+#ifeq ($(HOST_OS),linux)
+#  ifneq ($(TARGET_BUILD_VARIANT),eng)
+#    ifeq ($(WITH_DEXPREOPT),)
+#      WITH_DEXPREOPT := true
+#    endif
+#  endif
+#endif
+#WITH_DEXPREOPT_BOOT_IMG_ONLY ?= true
 
 # Display
 BOARD_USES_ADRENO := true
